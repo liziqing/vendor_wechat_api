4dc2dab46605a3c6cffa639e74fcce67c1ccb356
lvfeiyang <lxm19900207@163.com>
2017-05-04 16:11:35 +0800

    up

--- app/Http/Controllers/KangshifuController.php
+++ app/Http/Controllers/KangshifuController.php
@@ -1,1 +1,24 @@
-<?php/** * User: leon * Date: 2017/5/4 0004  下午 3:22 */namespace VendorWechat\Http\Controllers;use Illuminate\Http\Request;use VendorWechat\Util\Util;use Qiniu\Auth;class KangshifuController extends Controller{	public function getQnToken(Request $request)	{		$accessKey = Util::QINIU_ACCESS_KEY;		$secretKey = Util::QINIU_SECRET_KEY;		$auth = new Auth($accessKey, $secretKey);// 构建鉴权对象		$token = $auth->uploadToken('vendor-ads');		return Util::getSuccessJson("success", ['token'=>$token]);	}}
+<?php
+/**
+ * User: leon
+ * Date: 2017/5/4 0004  下午 3:22
+ */
+
+namespace VendorWechat\Http\Controllers;
+
+use Illuminate\Http\Request;
+use VendorWechat\Util\Util;
+use Qiniu\Auth;
+
+class KangshifuController extends Controller
+{
+	public function getQnToken(Request $request)
+	{
+		$accessKey = Util::QINIU_ACCESS_KEY;
+		$secretKey = Util::QINIU_SECRET_KEY;
+		$auth = new Auth($accessKey, $secretKey);// 构建鉴权对象
+		$token = $auth->uploadToken('vendor-ads');
+
+		return Util::getSuccessJson("success", ['token'=>$token]);
+	}
+}
