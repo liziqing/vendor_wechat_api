543f8a730282feb12509e09b4cb21befef39edb4
lvfeiyang <lxm19900207@163.com>
2017-05-08 10:08:08 +0800

    up

--- app/Http/Controllers/KangshifuController.php
+++ app/Http/Controllers/KangshifuController.php
@@ -29,6 +29,12 @@ class KangshifuController extends Controller
 			$cRedis->del($sHashKey);
 		}
 	}
+	private function getMobile(Request $req)
+	{
+//		$mobile = \Cookie::get(self::KSF_COOKIE, '');
+		$mobile = $req->input('mobile', '');
+		return $mobile;
+	}
 
 	public function getQnToken(Request $request)
 	{
@@ -52,12 +58,12 @@ class KangshifuController extends Controller
 			if (!empty($name))
 				$cRedis->hset(self::KSF_PREFIX.$mobile, self::USER_NAME, $name);
 		}
-		return Util::getSuccessJson("success", [])
-			->withCookie(cookie(self::KSF_COOKIE, $mobile, 0, '/', 'vendor.qnmami.com'));
+		return Util::getSuccessJson("success", []);
+//			->withCookie(cookie(self::KSF_COOKIE, $mobile, 0, '/', 'vendor.qnmami.com'));
 	}
 	public function getUserInfo(Request $req)
 	{
-		$mobile = \Cookie::get(self::KSF_COOKIE, '');
+		$mobile = $this->getMobile($req);
 		if (!empty($mobile))
 		{
 			$cRedis = \Redis::connection();
@@ -68,7 +74,7 @@ class KangshifuController extends Controller
 	}
 	public function getHuoLi(Request $req)
 	{
-		$mobile = \Cookie::get(self::KSF_COOKIE, '');
+		$mobile = $this->getMobile($req);
 		if (!empty($mobile))
 		{
 			$cRedis = \Redis::connection();
@@ -79,41 +85,144 @@ class KangshifuController extends Controller
 	}
 	public function postImageUp(Request $req)
 	{
-		$noCookie = false;
+//		$noCookie = false;
 		$cRedis = \Redis::connection();
-		$mobile = \Cookie::get(self::KSF_COOKIE, '');
+//		$mobile = \Cookie::get(self::KSF_COOKIE, '');
+		$mobile = $this->getMobile($req);
 		if (empty($mobile))
+			return Util::getErrorJson(ExceptionConstants::CODE_PARAM, "请填写手机号");
+		else
 		{
-			$firstMobile = $req->input('mobile', '');
-			if (empty($firstMobile))
-				return Util::getErrorJson(ExceptionConstants::CODE_PARAM, "请填写手机号");
+//			$firstMobile = $req->input('mobile', '');
+//			if (empty($firstMobile))
+//				return Util::getErrorJson(ExceptionConstants::CODE_PARAM, "请填写手机号");
 			//新建用户
-			if (!$cRedis->exists(self::KSF_PREFIX.$firstMobile))
-				$cRedis->hset(self::KSF_PREFIX.$firstMobile, self::USER_HUOLI, 0);
-			$noCookie = true;
+			if (!$cRedis->exists(self::KSF_PREFIX.$mobile))
+				$cRedis->hset(self::KSF_PREFIX.$mobile, self::USER_HUOLI, 0);
+//			$noCookie = true;
 		}
 
 		$type = $req->input('type', 0);//1订单 2活力时刻
 		$url = $req->input('url', '');
 
-		$zkey = self::KSF_PREFIX."$mobile:$type:1";//1待审核 不进墙
+		$zkey = self::KSF_PREFIX."$type:1:$mobile";//1待审核 不进墙
 		$timestamp = (new \DateTime("now"))->getTimestamp();
 		$cRedis->zadd($zkey, $timestamp, $url);
 
-		if ($noCookie)
+		return Util::getSuccessJson("success", []);
+		/*if ($noCookie)
 		{
-			return Util::getSuccessJson("success", [])
-				->withCookie(cookie(self::KSF_COOKIE, $mobile, 0, '/', 'vendor.qnmami.com'));
+			return Util::getSuccessJson("success", []);
+//				->withCookie(cookie(self::KSF_COOKIE, $mobile, 0, '/', 'vendor.qnmami.com'));
 		}
 		else
 		{
 			return Util::getSuccessJson("success", []);
+		}*/
+	}
+	private function getImageUrl($blurryKey)
+	{
+		$urlList = [];
+		$aPregOut = array();
+		$cRedis = \Redis::connection();
+		$zsetKeys = $cRedis->keys($blurryKey.'*');
+		foreach ($zsetKeys as $zsetKey)
+		{
+			preg_match("/^$blurryKey(\d*)$/", $zsetKey, $aPregOut);
+			if (!empty($aPregOut[1]))
+			{
+				$mobile = $aPregOut[1];
+				$urlList[] = [$mobile=>$cRedis->zrevrange($zsetKey, 0, -1)];
+			}
+//			$urlList = array_merge($urlList, $cRedis->zrevrange($zsetKey, 0, -1));
 		}
+		return $urlList;
 	}
 	public function getImageList(Request $req)
 	{
 		$type = $req->input('type', 0);//1用户的活力时刻 2活力墙 3非墙全活力 订单审核（4未 5已 6不）
 		$offset = $req->input('offset', 0);
 		$limit = $req->input('limit', 20);
+
+		$urlList = [];
+		$cRedis = \Redis::connection();
+		switch ($type)
+		{
+			case 1:
+			{
+				$mobile = $this->getMobile($req);
+				if (!empty($mobile))
+				{
+					$zkey1 = self::KSF_PREFIX."2:1:$mobile";
+					$zkey2 = self::KSF_PREFIX."2:2:$mobile";
+					$urlList = array_merge($urlList, $cRedis->zrevrange($zkey2, 0, -1));//byscore '+inf', '-inf'
+					$urlList = array_merge($urlList, $cRedis->zrevrange($zkey1, 0, -1));
+//					$zkeyu = self::KSF_PREFIX."2:$mobile";
+//					$cRedis->zunionstore($zkeyu,[$zkey1, $zkey2]);
+//					$cRedis->zrevrangebyscore($zkeyu, '+inf', '-inf');
+				}
+				break;
+			}
+			case 2:
+			{
+				$urlList = $this->getImageUrl(self::KSF_PREFIX."2:2:");//array_merge
+//				$zkeyu = self::KSF_PREFIX."2:2";
+				break;
+			}
+			case 3:
+			{
+				$urlList = $this->getImageUrl(self::KSF_PREFIX."2:1:");
+				break;
+			}
+
+			case 4:
+			{
+				$urlList = $this->getImageUrl(self::KSF_PREFIX."1:1:");
+				break;
+			}
+			case 5:
+			{
+				$urlList = $this->getImageUrl(self::KSF_PREFIX."1:2:");
+				break;
+			}
+			case 6:
+			{
+				$urlList = $this->getImageUrl(self::KSF_PREFIX."1:3:");
+				break;
+			}
+			default:
+			{
+				break;
+			}
+		}
+		return Util::getSuccessJson("success", ['list'=>$urlList]);
+	}
+	public function postChangeStatus(Request $req)
+	{
+		$url = $req->input('url', 0);
+		$type = $req->input('type', 0);//11 12 13 21 22
+		$mobile = $req->input('mobile', '');
+		$result = $req->input('result', 0); //1通过 2不通过
+
+		$cRedis = \Redis::connection();
+		$map = [
+			11=>[1=>[1,2], 2=>[1,3]],
+			12=>[1=>[2,2], 2=>[2,3]],
+			13=>[1=>[3,2], 2=>[3,3]],
+			21=>[1=>[1,2], 2=>[1,1]],
+			22=>[1=>[2,2], 2=>[2,1]],
+		];
+		$fromStatus = $map[$type][$result][0];
+		$toStatus = $map[$type][$result][1];
+		$fromKey = self::KSF_PREFIX."$type:$fromStatus:$mobile";
+		$toKey = self::KSF_PREFIX."$type:$toStatus:$mobile";
+
+		$timestamp = $cRedis->zscore($fromKey, $url);
+		$cRedis->zrem($fromKey, $url);
+		$cRedis->zadd($toKey, $timestamp, $url);
+	}
+	public function getManage(Request $req)
+	{
+		return view('kangshifu', []);
 	}
 }
---
+++ resources/views/kangshifu.blade.php
@@ -0,0 +1,46 @@
+<h1>康师傅活动审核</h1>
+<p>
+    <a href="javascript:void(0);" onclick="showlist('2')">照片墙</a>
+    <a href="javascript:void(0);" onclick="showlist('3')">未上墙</a>
+</p>
+<p>
+    <a href="javascript:void(0);" onclick="showlist('4')">带审核</a>
+    <a href="javascript:void(0);" onclick="showlist('5')">已通过</a>
+    <a href="javascript:void(0);" onclick="showlist('6')">不通过</a>
+</p>
+
+<div id="image_list"></div>
+
+<script type="text/javascript">
+    function showlist(type) {
+        $.ajax({
+            type:"get",
+            data:{type:type},
+            dataType:"json",
+            url:"/kangshifu/image-list",
+            success:function (data) {
+                var oneLen = 5;
+                var cyc = 0;
+                var coverLine = '';
+                var list = data.data.list;
+                for (var mobile in list)
+                {
+                    var oneData = list[mobile];
+                    for (var i in oneData)
+                    {
+                        coverLine += "<td><img src=oneData[i] title=mobile height='150'/>" +
+                            "<a href='javascript:void(0);' onclick='chgStatus(oneData[i], mobile, 1)'><i class='glyphicon glyphicon-ok'></i></a>" +
+                            "<a href='javascript:void(0);' onclick='chgStatus(oneData[i], mobile, 2)'><i class='glyphicon glyphicon-remove'></i></a></td>";
+
+                        if (oneLen <= cyc++)
+                        {
+                            $("#image_list").append("<tr>+coverLine+</tr>");
+                            cyc = 0;
+                            coverLine = '';
+                        }
+                    }
+                }
+            }
+        });
+    }
+</script>
