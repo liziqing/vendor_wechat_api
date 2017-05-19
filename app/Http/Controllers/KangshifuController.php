c604ef4792bf60fceb83c923cc8b591c21386c74
lvfeiyang <lxm19900207@163.com>
2017-05-19 11:22:19 +0800

    up

--- app/Http/Controllers/KangshifuController.php
+++ app/Http/Controllers/KangshifuController.php
@@ -438,7 +438,7 @@ class KangshifuController extends Controller
 
 					$turnTable = [20, 20, 50, 10]; //1、24号门票 2、21号门票 3、观看卷 4、未中奖
 					$havenFirst = $cRedis->get(self::KSF_PREFIX.self::HAVE_FIRST_PRIZE);
-					if ((!empty($havenFirst) && intval($havenFirst) >= 2) ||
+					if ((!empty($havenFirst) && intval($havenFirst) >= 4) ||
 						$cRedis->zscore(self::KSF_PREFIX.self::KSF_LOTTERY_PREFIX.$mobile, 1))
 					{
 						$turnTable[3] += $turnTable[0];
